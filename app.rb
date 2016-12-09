fe181eac548e7417596c60fc90f41282faccf29f
aznyqjai <aznyqjai@gmail.com>
2016-12-09 13:39:29 -0500

    create post works

--- app.rb
+++ app.rb
@@ -17,106 +17,107 @@ class App < Sinatra::Base
   enable :sessions
   register Sinatra::Flash
 
-get '/posts' do
-  @posts = Post.all
-  haml :posts
-end
-
-def current_user
-  if session[:user_id]
-    @current_user ||= User.find(session[:user_id])
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
   end
-end
 
-get '/' do
-  haml :home
-end
+  get '/' do
+    @posts = Post.all
+    haml :home
+  end
 
-get '/users' do
-  @users = User.all
-  haml :users
-end
+  get '/users' do
+    @users = User.all
+    haml :users
+  end
 
-get '/users/create' do
-  User.create(email: "Hadrian@gmail.com", first_name: "Hadrian", last_name: "Zhang")
-  haml :create
-end
+  get '/users/create' do
+    User.create(email: "Hadrian@gmail.com", first_name: "Hadrian", last_name: "Zhang")
+    haml :create
+  end
 
-get '/users/last' do
-  @user = User.last
-  haml :user
-end
+  get '/users/last' do
+    @user = User.last
+    haml :user
+  end
 
-get '/users/:id' do
-  @user = User.find(params[:id])
-  haml :user
-end
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    # @posts = Post.find()
+    haml :user
+  end
 
-get '/login' do
-  haml :login
-end
-
-get 'sessions/new' do
-  haml :login
-end
-
-post '/sessions/create' do
-  puts params.inspect
-  if @user= User.where(username: params[:username], password: params[:password]).first
-    puts "User LogIn"
-    session[:user_id] = @user.id
-   # haml :login_success
-    flash[:btn] = "you've succesfully logged in"
-    redirect '/'
-
-  else
-    @error = 'Login Failed'
-    puts "Login Error"
+  get '/login' do
     haml :login
   end
 
-#byebug
-end
+  get 'sessions/new' do
+    haml :login
+  end
 
-get '/logout' do
-  session[:user_id] = nil
-  redirect '/'
-end
+  post '/sessions/create' do
+    puts params.inspect
+    if @user= User.where(username: params[:username], password: params[:password]).first
+      puts "User LogIn"
+      session[:user_id] = @user.id
+    # haml :login_success
+      flash[:alert] = "you've succesfully logged in"
+      redirect '/'
+    else
+      @error = 'Login Failed'
+      puts "Login Error"
+      haml :login
+    end
+    #byebug
+  end
 
+  get '/logout' do
+    session[:user_id] = nil
+    redirect '/'
+  end
 
-get '/register' do
-  haml :register
-end
+  get '/register' do
+    haml :register
+  end
 
-get '/profile/edit' do
-  haml :edit
-end
+  get '/profile/edit' do
+    haml :edit
+  end
 
-post '/profile/update' do
-  puts params.inspect
-  # byebug
-  current_user.update_attributes(params[:user])
-  flash[:btn] = "you've succesfully update your profile"
-  redirect '/'
-end
+  post '/profile/update' do
+    puts params.inspect
+    # byebug
+    current_user.update_attributes(params[:user])
+    flash[:btn] = "you've succesfully update your profile"
+    redirect '/'
+  end
 
-post '/profile/delete' do
-  current_user.destroy
-  session[:user_id] = nil
-  flash[:btn] = "you've succesfully delete your account"
-  redirect '/'
-end
+  post '/profile/delete' do
+    current_user.destroy
+    session[:user_id] = nil
+    flash[:btn] = "you've succesfully delete your account"
+    redirect '/'
+  end
 
-post '/register' do
-  puts params.inspect
-  User.create(email: params[:email], username: params[:username], password: params[:password], first_name: params[:first_name], last_name: params[:last_name])
-  # flash[:notice] = "you've sucessfully registered!"
-  redirect '/login'
-end
+  post '/register' do
+    puts params.inspect
+    User.create(email: params[:email], username: params[:username], password: params[:password], first_name: params[:first_name], last_name: params[:last_name])
+    flash[:notice] = "you've sucessfully registered!"
+    redirect '/login'
+  end
 
-get '/posts' do
-  haml :posts
-end
+  get '/posts/create' do
+    haml :post
+  end
 
-run! if __FILE__ == $0
+  post '/posts/new' do
+    puts params.inspect
+    params[:post][:user_id] = current_user.id
+    Post.create(params[:post])
+    redirect '/'
+  end
+
+  run! if __FILE__ == $0
 end
